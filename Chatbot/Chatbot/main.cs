1715e7a79e5a45ca8c114cf4fe38e2f12ae34760
Kai <kai.oliver.k@web.de>
2025-08-06 10:52:43 +0200

    First steps, established connection to ollama api

---
+++ Chatbot/Chatbot/main.cs
@@ -0,0 +1,10 @@
+﻿string endpoint = "http://127.0.0.1:11434";
+HttpClient client = new HttpClient();
+
+client.BaseAddress = new Uri(endpoint);
+
+HttpResponseMessage availabilityResponse = await client.GetAsync("/api/tags");
+
+string stringResponse = await availabilityResponse.Content.ReadAsStringAsync();
+
+Console.WriteLine(stringResponse);
